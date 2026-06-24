cd6dc374bc58514216094e712a20237e0d6675ee
LeoLcde <leo.lacordaire@epitech.eu>
2026-06-24 14:58:48 +0200

    feat: right cmd

--- server/src/cmd/right.rs
+++ server/src/cmd/right.rs
@@ -2,13 +2,19 @@
  *  Project  -  cmd
  *  Date     -  June 10th 2026
  *
- *  Copyright (c) 2026 Jules Nourdin
+ *  Copyright (c) 2026 Jules Nourdin & Léo Lacordaire
  */
 
 use mio::Token;
+use crate::utils;
 use crate::utils::Server;
 
-pub fn cmd_right(token: Token, server: &mut Server)
-{
-    
+pub fn cmd_right(token: Token, server: &mut Server) {
+    let width = server.params.width;
+    let client = server.clients.get_mut(&token).unwrap();
+    let player = client.player.as_mut().unwrap();
+
+    player.x = (player.x + 1) % width;
+
+    let _ = utils::send_response(&mut client.stream, "ok\n");
 }
