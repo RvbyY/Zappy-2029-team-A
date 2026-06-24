bd4318caeee50b4d00fca3589f42b16e702609e7
LeoLcde <leo.lacordaire@epitech.eu>
2026-06-24 20:38:29 +0200

    feat: broadcast cmd

--- server/src/cmd/broadcast.rs
+++ server/src/cmd/broadcast.rs
@@ -10,5 +10,21 @@ use crate::utils::Server;
 
 pub fn cmd_broadcast(token: Token, server: &mut Server, _msg: String)
 {
-    
+    for (other_token, client) in server.clients.iter_mut() {
+        if *other_token == token {
+            continue;
+        }
+
+        let msg = format!("message 0, {}\n", _msg);
+        let _ = crate::utils::send_response(
+            &mut client.stream,
+            &msg
+        );
+    }
+
+    let sender = server.clients.get_mut(&token).unwrap();
+    let _ = crate::utils::send_response(
+        &mut sender.stream,
+        "ok\n"
+    );
 }
