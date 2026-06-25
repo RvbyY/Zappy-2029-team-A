f695d8872ab78573a06f8d0e1bfffc82df944f46
LeoLcde <leo.lacordaire@epitech.eu>
2026-06-25 22:54:39 +0200

    feat: implemente set & take cmd

--- server/src/cmd/set.rs
+++ server/src/cmd/set.rs
@@ -6,9 +6,24 @@
  */
 
 use mio::Token;
-use crate::utils::{Server, send_response, Direction};
+use crate::utils::{Server, send_response};
 
 pub fn cmd_set(token: Token, server: &mut Server, obj: String)
 {
+    let (x, y) = {
+        let player = server.clients.get(&token).unwrap().player.as_ref().unwrap();
+        (player.x as usize, player.y as usize)
+    };
 
+    let client = server.clients.get_mut(&token).unwrap();
+    let inv = client.player.as_mut().unwrap().inventory.entry(obj.clone()).or_insert(0);
+
+    if *inv > 0 {
+        *inv -= 1;
+        server.world.tiles[y][x].resources.entry(obj).and_modify(|v| *v += 1).or_insert(1);
+        let client = server.clients.get_mut(&token).unwrap();
+        let _ = send_response(&mut client.stream, "ok\n");
+    } else {
+        let _ = send_response(&mut client.stream, "ko\n");
+    }
 }
--- server/src/cmd/take.rs
+++ server/src/cmd/take.rs
@@ -6,9 +6,25 @@
  */
 
 use mio::Token;
-use crate::utils::{Server, send_response, Direction};
+use crate::utils::{Server, send_response};
 
 pub fn cmd_take(token: Token, server: &mut Server, obj: String)
 {
+    let (x, y) = {
+        let player = server.clients.get(&token).unwrap().player.as_ref().unwrap();
+        (player.x as usize, player.y as usize)
+    };
 
+    let tile = &mut server.world.tiles[y][x];
+    let count = tile.resources.entry(obj.clone()).or_insert(0);
+
+    if *count > 0 {
+        *count -= 1;
+        let client = server.clients.get_mut(&token).unwrap();
+        *client.player.as_mut().unwrap().inventory.entry(obj).or_insert(0) += 1;
+        let _ = send_response(&mut client.stream, "ok\n");
+    } else {
+        let client = server.clients.get_mut(&token).unwrap();
+        let _ = send_response(&mut client.stream, "ko\n");
+    }
 }
