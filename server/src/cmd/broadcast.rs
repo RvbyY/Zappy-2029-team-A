f50968c4599d79e6dbc01f22642b9e9dc36cc3bf
LeoLcde <leo.lacordaire@epitech.eu>
2026-06-26 15:24:27 +0200

    refacto: more clean up code

--- server/src/cmd/broadcast.rs
+++ server/src/cmd/broadcast.rs
@@ -6,25 +6,26 @@
  */
 
 use mio::Token;
-use crate::utils::Server;
+use crate::utils::{Server, send_result};
 
-pub fn cmd_broadcast(token: Token, server: &mut Server, _msg: String)
+fn broadcast_to_others(token: Token, server: &mut Server, msg: &str)
 {
     for (other_token, client) in server.clients.iter_mut() {
         if *other_token == token {
             continue;
         }
 
-        let msg = format!("message 0, {}\n", _msg);
         let _ = crate::utils::send_response(
             &mut client.stream,
-            &msg
+            msg
         );
     }
+}
 
-    let sender = server.clients.get_mut(&token).unwrap();
-    let _ = crate::utils::send_response(
-        &mut sender.stream,
-        "ok\n"
-    );
+pub fn cmd_broadcast(token: Token, server: &mut Server, msg: String)
+{
+    let formatted = format!("message 0, {}\n", msg);
+
+    broadcast_to_others(token, server, &formatted);
+    send_result(token, server, "ok");
 }
--- server/src/cmd/eject.rs
+++ server/src/cmd/eject.rs
@@ -8,14 +8,59 @@
 use mio::Token;
 use crate::utils::{Server, send_response, Direction};
 
-pub fn cmd_eject(token: Token, server: &mut Server)
+fn get_player_infos(token: Token, server: &Server) -> (u32, u32, Direction)
 {
-    let (x, y, direction) = {
-        let client = server.clients.get(&token).unwrap();
-        let player = client.player.as_ref().unwrap();
-        (player.x, player.y, player.direction.clone())
+    let client = server.clients.get(&token).unwrap();
+    let player = client.player.as_ref().unwrap();
+
+    (player.x, player.y, player.direction.clone())
+}
+
+fn next_tile(x: u32, y: u32, direction: &Direction, server: &Server,) -> (u32, u32)
+{
+    match direction {
+        Direction::N => (x, (y + server.params.height - 1) % server.params.height),
+        Direction::S => (x, (y + 1) % server.params.height),
+        Direction::E => ((x + 1) % server.params.width, y),
+        Direction::W => ((x + server.params.width - 1) % server.params.width, y),
+    }
+}
+
+fn move_players(server: &mut Server, players: &[Token], x: u32, y: u32, new_x: u32, new_y: u32)
+{
+    for token in players {
+        server.world.tiles[y as usize][x as usize].players.retain(|&t| t != *token);
+
+        server.world.tiles[new_y as usize][new_x as usize].players.push(*token);
+
+        let player = server.clients.get_mut(token).unwrap().player.as_mut().unwrap();
+
+        player.x = new_x;
+        player.y = new_y;
+    }
+}
+
+fn notify_players(server: &mut Server, players: &[Token], direction: &Direction)
+{
+    let dir = match direction {
+        Direction::N => Direction::S.to_num(),
+        Direction::S => Direction::N.to_num(),
+        Direction::E => Direction::W.to_num(),
+        Direction::W => Direction::E.to_num(),
     };
 
+    for token in players {
+        let client = server.clients.get_mut(token).unwrap();
+        let _ = send_response(
+            &mut client.stream,
+            &format!("eject: {}\n", dir),
+        );
+    }
+}
+
+pub fn cmd_eject(token: Token, server: &mut Server)
+{
+    let (x, y, direction) = get_player_infos(token, server);
     let others: Vec<Token> = server.world.tiles[y as usize][x as usize].players.iter().filter(|&&t| t != token).copied().collect();
 
     if others.is_empty() {
@@ -24,34 +69,9 @@ pub fn cmd_eject(token: Token, server: &mut Server)
         return;
     }
 
-    let (new_x, new_y) = match direction {
-        Direction::N => (x, if y == 0 { server.params.height - 1 } else { y - 1 }),
-        Direction::S => (x, (y + 1) % server.params.height),
-        Direction::E => ((x + 1) % server.params.width, y),
-        Direction::W => (if x == 0 { server.params.width - 1 } else { x - 1 }, y),
-    };
-
-    for other_token in &others {
-        // maj tile
-        server.world.tiles[y as usize][x as usize].players.retain(|&t| t != *other_token);
-        server.world.tiles[new_y as usize][new_x as usize].players.push(*other_token);
-        // maj player
-        let other_player = server.clients.get_mut(other_token).unwrap().player.as_mut().unwrap();
-        other_player.x = new_x;
-        other_player.y = new_y;
-    }
-
-    let dir = match direction {
-        Direction::N => Direction::S.to_num(),
-        Direction::S => Direction::N.to_num(),
-        Direction::E => Direction::W.to_num(),
-        Direction::W => Direction::E.to_num(),
-    };
-
-    for other_token in &others {
-        let client = server.clients.get_mut(other_token).unwrap();
-        let _ = send_response(&mut client.stream, &format!("eject: {}\n", dir));
-    }
+    let (new_x, new_y) = next_tile(x, y, &direction, server);
+    move_players(server, &others, x, y, new_x, new_y);
+    notify_players(server, &others, &direction);
 
     let client = server.clients.get_mut(&token).unwrap();
     let _ = send_response(&mut client.stream, "ok\n");
