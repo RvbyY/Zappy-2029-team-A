ea513f174c4554c1a922f458c93a83a2a61c8a95
LeoLcde <leo.lacordaire@epitech.eu>
2026-06-26 15:29:40 +0200

    refacto: clean up forward

--- server/src/cmd/forward.rs
+++ server/src/cmd/forward.rs
@@ -6,31 +6,44 @@
  */
 
 use mio::Token;
-use crate::utils::{Server, send_response, Direction};
+use crate::utils::{Direction, Server, send_result};
+
+fn get_movement(token: Token, server: &Server) -> (u32, u32, u32, u32)
+{
+    let client = server.clients.get(&token).unwrap();
+    let player = client.player.as_ref().unwrap();
+
+    let ox = player.x;
+    let oy = player.y;
+
+    let (nx, ny) = match player.direction {
+        Direction::N => (ox, if oy == 0 { server.params.height - 1 } else { oy - 1 }),
+        Direction::S => (ox, (oy + 1) % server.params.height),
+        Direction::E => ((ox + 1) % server.params.width, oy),
+        Direction::W => (if ox == 0 { server.params.width - 1 } else { ox - 1 }, oy),
+    };
+
+    (ox, oy, nx, ny)
+}
+
+fn update_player_position(token: Token, server: &mut Server, x: u32, y: u32)
+{
+    let client = server.clients.get_mut(&token).unwrap();
+    let player = client.player.as_mut().unwrap();
+
+    player.x = x;
+    player.y = y;
+}
 
 pub fn cmd_forward(token: Token, server: &mut Server)
 {
-    let (old_x, old_y, new_x, new_y) = {
-        let client = server.clients.get(&token).unwrap();
-        let player = client.player.as_ref().unwrap();
-        let (ox, oy) = (player.x, player.y);
-        let (nx, ny) = match player.direction {
-            Direction::N => (ox, if oy == 0 { server.params.height - 1 } else { oy - 1 }),
-            Direction::S => (ox, (oy + 1) % server.params.height),
-            Direction::E => ((ox + 1) % server.params.width, oy),
-            Direction::W => (if ox == 0 { server.params.width - 1 } else { ox - 1 }, oy),
-        };
-        (ox, oy, nx, ny)
-    };
+    let (old_x, old_y, new_x, new_y) = get_movement(token, server);
 
-    // change le token pour le nouveau
+    // déplacement sur la map
     server.world.tiles[old_y as usize][old_x as usize].players.retain(|&t| t != token);
     server.world.tiles[new_y as usize][new_x as usize].players.push(token);
 
-    let client = server.clients.get_mut(&token).unwrap();
-    let player = client.player.as_mut().unwrap();
-    player.x = new_x;
-    player.y = new_y;
+    update_player_position(token, server, new_x, new_y);
 
-    let _ = send_response(&mut client.stream, "ok\n");
+    send_result(token, server, "ok");
 }
