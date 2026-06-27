f5235c4a13c3f8bd035d8f784d044a0c920999ee
LeoLcde <leo.lacordaire@epitech.eu>
2026-06-28 01:35:53 +0200

    fix: broadcast gui connected

--- server/src/cmd/broadcast.rs
+++ server/src/cmd/broadcast.rs
@@ -6,12 +6,12 @@
  */
 
 use mio::Token;
-use crate::utils::{Server, send_result};
+use crate::utils::{Server, send_result, notify_gui};
 
 fn broadcast_to_others(token: Token, server: &mut Server, msg: &str)
 {
     for (other_token, client) in server.clients.iter_mut() {
-        if *other_token == token {
+        if *other_token == token || client.is_gui {
             continue;
         }
 
@@ -24,8 +24,11 @@ fn broadcast_to_others(token: Token, server: &mut Server, msg: &str)
 
 pub fn cmd_broadcast(token: Token, server: &mut Server, msg: String)
 {
+    let n = token.0 as u32;
     let formatted = format!("message 0, {}\n", msg);
 
     broadcast_to_others(token, server, &formatted);
+
     send_result(token, server, "ok");
+    notify_gui(&mut server.clients, &format!("pbc #{} {}\n", n, msg));
 }
